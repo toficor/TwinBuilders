56a3ce8fcc1820a9ee87f368c5d8fe6ac82f08ac
Sebastian Łużny <sebastianluzny9@gmail.com>
2021-10-17 13:57:11 +0200

    start working on grid

---
+++ Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Grid
+{
+    private int _width;
+    private int _height;
+    private float _cellSize;
+   
+
+   // public Grid(int width, int height, float cellSize)
+}
